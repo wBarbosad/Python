4e3c444fdb4071b77fc02699966004142173ed70
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-03-02 19:19:00 -0300

    Inicialização Python e Sintaxes

---
+++ Scripts_Python/Exercicio1_1.py
@@ -0,0 +1,6 @@
+nome = input("Informe o seu nome: ")
+print("Seja bem-vindo(a), Sr.", nome)
+dia = input("Informe o dia do seu nascimento: ")
+mes = input("Informe o mês do seu nascimento: ")
+ano = input("Informe o ano do seu nascimento: ")
+print("Você nasceu no dia", dia, "de", mes, "de", ano, ". Correto?")
---
+++ Scripts_Python/Exercicio1_2.py
@@ -0,0 +1,4 @@
+numero1 = input("Infore um número = ")
+numero2 = input("Informe o segundo número = ")
+soma = numero1+numero2
+print("A soma dos dois números = ", soma)
